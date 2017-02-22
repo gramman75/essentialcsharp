5e36541f036f7171cffeb256d99c81384a77f794
gramman75 <gramman75@gmail.com>
2017-02-22 18:04:43 +0900

    working

---
+++ Chapter01/GInterface.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter01
+{
+    interface ITest1
+    {
+        void Test1();
+    }
+
+    interface ITest2 : ITest1
+    {
+        void Test2();
+    }
+    class GInterface: ITest2
+    {
+        void ITest1.Test1()
+        {
+
+        }
+
+        public void Test2()
+        { 
+
+        }
+
+    }
+
+    interface IReadableSettingsProvider
+    {
+        string GetSetting(string name, string defaultValue);
+    }
+
+    interface ISettingsProvider : IReadableSettingsProvider
+    {
+        void SetSetting(string name, string value);
+    }
+
+    public class FileSettingProvider : IReadableSettingsProvider, ISettingsProvider
+    {
+
+        // 명시적 구현시 해당 Member가 속해 있는 Interface
+        // 명시적 구현시에는 Modifier사용 불가
+        string IReadableSettingsProvider.GetSetting(string name, string defaultValue)
+        {
+            return "a";
+        }
+
+        public void SetSetting(string name, string value)
+        {
+        }
+
+    }
+
+
+        
+
+}
--- Chapter01/Program.cs
+++ Chapter01/Program.cs
@@ -7,11 +7,11 @@ namespace Chapter01
 {
     class PdaItem
     {
-        
+
 
         public string Name { get; set; } = "Gramman Name";
         public string ID { get; set; } = "123456";
-   
+
         public virtual void PrintDeviceName()
         {
             Console.WriteLine($"PDA Device Name={Name}");
@@ -43,6 +43,7 @@ namespace Chapter01
 
         static void Main(string[] args)
         {
+            Console.ReadLine();
 
             Contact contact = new Contact();
 
@@ -50,5 +51,28 @@ namespace Chapter01
 
             contact.PrintDeviceID();
         }
+
+        
     }
+
+    public abstract class Sample: ITest{
+        // abstact 형한정자가 선언이되면 구현을 하지 않아도 됨.
+        public abstract int Test1();
+        // abstract 형한저자가 없으면 아래와 같이 exception이라도 구현해야 함.
+        public int Test2()
+        {
+            throw new NotImplementedException();
+        }
+            
+    }
+
+
+    interface ITest
+    {
+        int Test1();
+        int Test2();        
+
+    }
+
+    
 }
